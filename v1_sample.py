498dca5ae78c252697cc82d9ae46c6c1fab323ee
Paul Ling <xuehai.ling@gmail.com>
2025-03-05 22:20:58 -0500

    Create v1_sample.py

---
+++ v1_sample.py
@@ -0,0 +1,10 @@
+import streamlit.components.v1 as components
+
+components.html(
+    """
+    <div style='color: red;'>
+        <h1>Hello, Streamlit!</h1>
+    </div>
+    """,
+    height=200,
+)
